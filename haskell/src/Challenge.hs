f4a31a08a823112574d03071bf74541359f75ec5
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2015-06-20 00:16:04 +0200

    Added haskell code for Challenge 3.

--- haskell/src/Challenge.hs
+++ haskell/src/Challenge.hs
@@ -4,10 +4,13 @@ module Challenge where
     let msg = "Hello world!"
     c1 msg
     c2 msg
+    c3 msg
     return ()
 
   c1 msg = putStrLn msg
 
   c2 msg = let msges = msg : msges
             in mapM (c1) $ take 2 msges
+
+  c3 msg = c2 $ reverse msg
 
